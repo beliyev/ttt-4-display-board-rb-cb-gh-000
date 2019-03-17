23dd2ffd025239dde78303ac3439c88763e861f7
Ilkin Babayev <beliyev1@std.beu.edu.az>
2019-03-17 18:59:46 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 board = [" "," "," "," ","X"," "," "," "," "]
 def display_board(board)
-    puts "   | X |   "
+    puts "   |   |   "
     puts "-----------"
     puts "   | X |   "
     puts "-----------"
